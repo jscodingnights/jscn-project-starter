fe93a492808f91558a3fd054da239573d1d4e07c
Adam Terlson <adam.terlson@internations.org>
2016-08-28 21:57:05 +0200

    POST PUT and DELETE on candidates

--- app/index.js
+++ app/index.js
@@ -1,28 +1,31 @@
+import express from 'express';
 import jsonServer from 'json-server';
 import db from './db';
 const server = jsonServer.create();
 const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
+const denied = (req, res) => res.send(403, 'This endpoint is read/create (GET)');
+const readOnlyRoute = () => {
+    const router = express.Router();
+    router.put('/', denied);
+    router.delete('/', denied);
+    return router;
+};
+
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
 
 // Add custom routes before JSON Server router
-server.get('/echo', function (req, res) {
+server.get('/echo', (req, res) => {
   res.jsonp(req.query);
 });
 
-server.use(function (req, res, next) {
-  if (req.method === 'POST') {
-    req.body.createdAt = Date.now();
-  }
-  // Continue to JSON Server router
-  next();
-});
+server.use('/candidates/:id?', readOnlyRoute());
 
 // Use default router
 server.use(router);
-server.listen(3000, function () {
+server.listen(3000, () => {
   console.log('JSON Server is running');
 });
 
--- test/app.test.js
+++ test/app.test.js
@@ -3,29 +3,50 @@ import expect from 'expect';
 import request from 'supertest';
 import app from '../app';
 
-const arrayFixedIds = (res) => {
-    res.body = res.body.map((i, id) => ({ ...i, id}));
-}
-
 describe('/candidates', () => {
-    it('GET / => list of candidates', (done) => {
+    it('GET /candidates => list of candidates', (done) => {
         request(app)
             .get('/candidates')
             .expect('Content-Type', /json/)
-            .expect(arrayFixedIds)
             .expect(200, [
                 {
-                    id: 0,
+                    id: 1,
                     name: 'Hillary Clinton',
                     photoUrl: 'http://www.newsbiscuit.com/wp-content/uploads/2015/10/hillary-clinton-womenjpeg-045d7.jpg',
                 },
                 {
-                    id: 1,
+                    id: 2,
                     name: 'Donald Trump',
                     photoUrl: 'http://images.huffingtonpost.com/2016-07-15-1468607338-43291-DonaldTrumpangry.jpg',
                 }
             ])
             .end(done);
     });
-
+    it('POST /candidates => create new candidate', (done) => {
+        request(app)
+            .post('/candidates')
+            .send({ name: 'My Candidate' })
+            .expect(res => {
+                expect(res.body.name).toEqual('My Candidate');
+                expect(res.body.id).toEqual(3);
+            })
+            .end(done);
+    });
+    it('PUT /candidates/1 => should not update hillary', (done) => {
+        request(app)
+            .put('/candidates/1')
+            .send({ name: 'New Hillary' })
+            .expect(res => {
+                expect(res.status).toEqual(403);
+            })
+            .end(done);
+    });
+    it('DELETE /candidates/1 => should not delete hillary', (done) => {
+        request(app)
+            .delete('/candidates/1')
+            .expect(res => {
+                expect(res.status).toEqual(403);
+            })
+            .end(done);
+    });
 });
