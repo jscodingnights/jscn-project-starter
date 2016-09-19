3d2ec5edfd21e1b3953b4571421d051a5a48b091
Adam Terlson <adam.terlson@internations.org>
2016-09-19 21:38:57 +0200

    wtf

---
+++ index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
