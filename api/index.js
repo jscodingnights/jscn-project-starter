835d1a77177e6e4c2385211d92c54d03d3f8a68a
Adam Terlson <adam.terlson@internations.org>
2016-09-19 21:33:00 +0200

    wtf

--- api/index.js
+++ api/index.js
@@ -1,27 +1,121 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import http from 'http';
-import usPresidents from './us-presidents';
-import chat from './chat';
+// Add welcome message
+//
 
-const app = express();
-const server = http.createServer(app);
-const io = require('socket.io')(server);
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, "0.0.0.0");
+console.log('on ', port);
+
+var router = express.Router();
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/' + (process.env.ADMIN_ROUTE || 'admin'), function(req, res){
+  res.sendFile(__dirname + '/admin.html');
+});
+
+router.post('/', function (req, res) {
+  if (req.body && req.body.author && req.body.text) {
+    messages.push(req.body);
+  }
+  res.json(req.body);
+});
 
 app.use(bodyParser.json());
-app.use('/api/us-presidents', usPresidents);
-app.use('/api/chat', chat(io));
+app.use(function (req, res, next) {
+  console.log('req');
+  next();
+});
+app.use(router);
 
-// Use default router
-app.use((err, req, res, next) => {
-    return res.send(500, {
-        success: false,
-        message: err.message,
+
+function emit(socket, name, data) {
+  console.log('EMITTING', name, data);
+
+  socket.emit('action', Object.assign({}, data, {
+    type: name,
+  }));
+  socket.emit(name, data);
+}
+
+function on(socket, name, cb) {
+  socket.on(name, cb);
+  socket.on('action', (data) => {
+    if (data.type === name) {
+      cb(data);
+    }
+  })
+}
+
+// Chatroom
+
+// members which are currently connected to the chat
+var members = [];
+var usernameNum = 0;
+
+io.on('connection', function (socket) {
+  console.log('connected!');
+
+  socket.user = {
+    username: 'Anonymous',
+    created: (new Date).getTime()
+  };
+
+  // add the client's username to the global list
+  members.push(socket.user);
+
+  emit(socket, 'RECEIVE_MEMBERS', { members });
+  emit(socket.broadcast, 'RECEIVE_MEMBERS', { members });
+  emit(socket.broadcast, 'MEMBER_JOIN', { user: socket.user });
+
+  // when the client emits 'new message', this listens and executes
+  on(socket, 'CREATE_MESSAGE', function (data) {
+    var message = data && data.message;
+
+    Object.assign(message, {
+      text: message.text,
+      author: socket.user || { username: 'Anonymous' },
+      date: (new Date).getTime()
     });
-});
 
-server.listen(process.env.PORT || 3000, '0.0.0.0', () => {
-  console.log('JSCN Server is running: 3000');
-});
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('action', { type: 'RECEIVE_MESSAGE', message });
+    socket.broadcast.emit('RECEIVE_MESSAGE', { type: 'RECEIVE_MESSAGE', message });
+  });
 
-export default app;
+  // when the client emits 'UPDATE_USER', this listens and executes
+  on(socket, 'UPDATE_USER', function (event) {
+    console.log(event);
+    if (!event || !event.user || !event.user.username) {
+      return;
+    }
+
+    var userUpdate = {
+      username: event.user.username
+    };
+
+    var oldUser = Object.assign({}, socket.user);
+    var user = Object.assign(socket.user, userUpdate);
+
+    emit(socket, 'RECEIVE_USER', { user });
+    emit(socket.broadcast, 'USER_UPDATE', { oldUser, user });
+    emit(socket, 'RECEIVE_MEMBERS', { members });
+    emit(socket.broadcast, 'RECEIVE_MEMBERS', { members });
+  });
+
+  on(socket, 'disconnect', function () {
+    members = members.filter((user) => {
+      return user !== socket.user;
+    });
+
+    // echo globally that this client has left
+    emit(socket.broadcast, 'MEMBER_LEAVE', { user: socket.user });
+    emit(socket.broadcast, 'RECEIVE_MEMBERS', { members });
+  });
+});
